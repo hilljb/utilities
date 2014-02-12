e864953c355321f8fd7fb97072f29aebb9fe901f
hilljb <jason@jasonbhill.com>
2014-02-11 23:36:25 -0700

    cleaned up, moved to uint32_t only

--- linkedList/cpp/basic/linkedListBasic.cpp
+++ linkedList/cpp/basic/linkedListBasic.cpp
@@ -2,69 +2,71 @@
 /*
  *  linkedListBasic.cpp
  *
- *  utilities library - basic C++ linked list
- *
+ *  utilities library - basic C++ linked list (data type: uint32_t)
  *  Copyright (C) 2014  Jason B. Hill (jason@jasonbhill.com)
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
+ *  This library is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or (at
+ *  your option) any later version.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ *  This library is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ *  License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
+#include <cstdint>
 
 #include "linkedListBasic.hpp"
 
 namespace utilities { namespace linkedList {
 
+
 // construct a basic linked list
 BasicLinkedList::BasicLinkedList() {
-    // allocate the head and tail
-    head = new node_t;
-    tail = new node_t;
+    // allocate the head of the list
+    head = new Head32_t;
 
-    // point the head to the tail and point the tail to the head
-    head->next = tail;
-    tail->next = head;
-
-    // make the objects of the head and tail NULL
-    head->obj = 0;
-    tail->obj = 0;
+    // point the head to NULL in both directions
+    head->next = 0;
+    head->last = 0;
 }
 
-// destruct a basic linked list (the objects at each node should be cleared)
+// destruct a basic linked list
 BasicLinkedList::~BasicLinkedList() {
-    // delete middle node structs here
+    // delete inner nodes
 
-    // delete the head and tail structs
+    // delete the head
     delete(head);
-    delete(tail);
 }
 
-// append an item to a linked list
-void BasicLinkedList::append(void* obj) {
+// append an item to a basic linked list
+void BasicLinkedList::append(const uint32_t& d) {
+    Node32_t*       newNode;
+
     // allocate a new node
-    node new_node = new node_t;
+    newNode = new Node32_t;
 
-    // store obj pointer in node
-    new_node->obj = obj;
+    // set the data component of the new node
+    newNode->data = d;
 
-    // set links
-    // reset final link as follows:
-    // 1) reset the link from tail to the last non-trivial node
-    tail->next->next = new_node;
-    // 2) reset the link from the last non-trivial node
-    tail->next = new_node;
-    // 3) make the new last node point to the tail
-    new_node->next = tail;
+    // set the next pointer of the new node
+    newNode->next = 0;
+
+    // append to end of the list
+    if (head->next == 0) { // list is empty
+        head->next = newNode;
+        head->last = newNode;
+    }
+    else { // list already has a non-head node
+        head->last->next = newNode;
+        head->last = newNode;
+    }
 }
 
 
